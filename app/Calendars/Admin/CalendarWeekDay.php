29dc22d2e6d427a3e6c4b21899e132c28a484abf
SushiYui <1agifurai@gmail.com>
2025-01-13 18:02:13 +0900

    スクール予約確認画面の各部数にリンクを設置する

--- app/Calendars/Admin/CalendarWeekDay.php
+++ app/Calendars/Admin/CalendarWeekDay.php
@@ -32,13 +32,13 @@ class CalendarWeekDay{
 
     $html[] = '<div class="text-left">';
     if($one_part){
-      $html[] = '<p class="day_part m-0 pt-1">1部　　'. 20 - $one_part->limit_users .'</p>';
+      $html[] = '<p class="day_part m-0 pt-1"><a href="' .route('calendar.admin.detail', ['date' => $one_part->setting_reserve, 'part' =>$one_part->setting_part]). '">1部</a>　　'. 20 - $one_part->limit_users .'</p>';
     }
     if($two_part){
-      $html[] = '<p class="day_part m-0 pt-1">2部　　'. 20 - $two_part->limit_users .'</p>';
+      $html[] = '<p class="day_part m-0 pt-1"><a href="' .route('calendar.admin.detail', ['date' => $two_part->setting_reserve, 'part' =>$two_part->setting_part]). '">1部</a>　　'. 20 - $one_part->limit_users .'</p>';
     }
     if($three_part){
-      $html[] = '<p class="day_part m-0 pt-1">3部　　'. 20 - $three_part->limit_users .'</p>';
+      $html[] = '<p class="day_part m-0 pt-1"><a href="' .route('calendar.admin.detail', ['date' => $three_part->setting_reserve, 'part' =>$three_part->setting_part]). '">3部</a>　　'. 20 - $three_part->limit_users .'</p>';
     }
     $html[] = '</div>';
 
